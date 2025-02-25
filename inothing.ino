fed3c7fd1172efc5f1e8ee6bd089133986c88757
Yuk <86674283+37saku@users.noreply.github.com>
2025-02-25 16:36:35 +0800

    Update inothing.ino

--- inothing.ino
+++ inothing.ino
@@ -1,34 +1,182 @@
-// ...existing code...
+// For ESP8266 update pin definitions (using NodeMCU pin labels)
+const int dirPin = 5;    // direction control pin for ESP8266
+const int stepPin = 4;   // step control pin for ESP8266
 
-void runStepper(int steps, bool direction) {
-  digitalWrite(dirPin, direction);  // Set direction
-  
-  for(int x = 0; x < steps; x++) {
+const int moveSteps = 200;    // test steps
+char cmd;
+int data;
+int motorSpeed = 1000;
+
+#include <SoftwareSerial.h>
+
+// For ESP8266 update pin definitions (using NodeMCU pin labels)
+const int dirPin = 5;    // direction control pin for ESP8266
+const int stepPin = 4;   // step control pin for ESP8266
+const int rxPin = 12;    // RX pin for software serial
+const int txPin = 13;    // TX pin for software serial
+
+const int moveSteps = 200;    // test steps
+char cmd;
+int data;
+int motorSpeed = 1000;
+
+SoftwareSerial mySerial(rxPin, txPin); // RX, TX
+
+void setup() {
+  pinMode(stepPin, OUTPUT);      // Set step pin as output
+  pinMode(dirPin, OUTPUT);       // Set direction pin as output
+  pinMode(rxPin, INPUT);
+  pinMode(txPin, OUTPUT);
+
+  mySerial.begin(9600);
+  mySerial.println("++++++++ ESP8266 Single-Stepper Demo ++++++++");
+  mySerial.println("Please input motor command:");
+}
+
+void loop() {
+  if (mySerial.available()) {
+    cmd = mySerial.read();
+    mySerial.print("cmd = ");
+    mySerial.print(cmd);
+    mySerial.print(" , ");
+    data = mySerial.parseInt();
+    mySerial.print("data = ");
+    mySerial.println(data);
+    runUsrCmd();
+  }
+}
+
+// Updated runUsrCmd to use mySerial instead of Serial
+void runUsrCmd(){
+  switch(cmd){ 
+    case 'x':  
+      mySerial.print("Set Rotation To ");
+      if (data == 0){
+        digitalWrite(dirPin, HIGH);
+        mySerial.println("Counter Clockwise.");
+      } else {
+        digitalWrite(dirPin, LOW);
+        mySerial.println("Clockwise.");
+      }
+      break;
+      
+    case 'U': 
+      mySerial.print("Float Up with steps = ");
+      mySerial.println(data);
+      digitalWrite(dirPin, HIGH);
+      runStepper(motorSpeed, data);
+      break;
+      
+    case 'D': 
+      mySerial.print("Float Down with steps = ");
+      mySerial.println(data);
+      digitalWrite(dirPin, LOW);
+      runStepper(motorSpeed, data);
+      break;
+      
+    case 'z':
+      runStepper(motorSpeed, data);
+      break;
+      
+    case 'd':
+      motorSpeed = data;
+      mySerial.print("Set Motor Speed To ");
+      mySerial.println(data);
+      break;
+      
+    case '!':
+      mySerial.println("Emergency Stop!");
+      break;
+  }
+}
+
+void runStepper (int rotationSpeed, int stepNum){
+  for(int x = 0; x < stepNum; x++) {
+    if (mySerial.available() && mySerial.peek() == '!') {
+      mySerial.read();
+      mySerial.println("Emergency Stop!");
+      break;
+    }
     digitalWrite(stepPin, HIGH);
-    delayMicroseconds(motorSpeed);
+    delayMicroseconds(rotationSpeed);
+    yield();
     digitalWrite(stepPin, LOW);
-    delayMicroseconds(motorSpeed);
+    delayMicroseconds(rotationSpeed);
+    yield();
   }
 }
 
-void runUsrCmd() {
-  switch(cmd) {
-    case 'F':  // Forward
-      runStepper(data, HIGH);
+void loop() {
+  if (Serial.available()) {
+    cmd = Serial.read();
+    Serial.print("cmd = ");
+    Serial.print(cmd);
+    Serial.print(" , ");
+    data = Serial.parseInt();
+    Serial.print("data = ");
+    Serial.println(data);
+    runUsrCmd();
+  }
+}
+
+// Updated: runUsrCmd without CNC hat commands
+void runUsrCmd(){
+  switch(cmd){ 
+    case 'x':  // Set motor rotation direction
+      Serial.print("Set Rotation To ");
+      if (data == 0){
+        digitalWrite(dirPin, HIGH);  // swapped: now HIGH means Counter Clockwise
+        Serial.println("Counter Clockwise.");
+      } else {
+        digitalWrite(dirPin, LOW);   // LOW means Clockwise
+        Serial.println("Clockwise.");
+      }
       break;
       
-    case 'B':  // Backward
-      runStepper(data, LOW);
+    // New case for upward movement
+    case 'U': 
+      Serial.print("Float Up with steps = ");
+      Serial.println(data);
+      digitalWrite(dirPin, HIGH);
+      runStepper(motorSpeed, data);
       break;
       
-    case 'S':  // Set Speed (delay in microseconds)
-      if(data > 0) motorSpeed = data;
+    // New case for downward movement
+    case 'D': 
+      Serial.print("Float Down with steps = ");
+      Serial.println(data);
+      digitalWrite(dirPin, LOW);
+      runStepper(motorSpeed, data);
       break;
       
-    default:
-      Serial.println("Invalid command");
+    case 'z': // Run stepper for a number of steps using set motor speed
+      runStepper(motorSpeed, data);
+      break;
+      
+    case 'd': // Set motor step delay speed
+      motorSpeed = data;
+      Serial.print("Set Motor Speed To ");
+      Serial.println(data);
+      break;
+      
+    case '!': // Emergency Stop command
+      Serial.println("Emergency Stop!");
       break;
   }
 }
 
-// ...existing code...
+void runStepper (int rotationSpeed, int stepNum){
+  for(int x = 0; x < stepNum; x++) {
+    if (Serial.available() && Serial.peek() == '!') {
+      Serial.read();  // consume emergency stop command
+      Serial.println("Emergency Stop!");
+      break;
+    }
+    digitalWrite(stepPin, HIGH);
+    delayMicroseconds(rotationSpeed);
+    yield(); // allow watchdog feeding
+    digitalWrite(stepPin, LOW);
+    delayMicroseconds(rotationSpeed);
+    yield(); // allow watchdog feeding
+  }
+}
