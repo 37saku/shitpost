0d5e9046f4742563decc4b2e5f8b52c099d590ce
Yuk <86674283+37saku@users.noreply.github.com>
2025-02-26 16:30:15 +0800

    Update inothing.ino

--- inothing.ino
+++ inothing.ino
@@ -1,12 +1,3 @@
-// For ESP8266 update pin definitions (using NodeMCU pin labels)
-const int dirPin = 5;    // direction control pin for ESP8266
-const int stepPin = 4;   // step control pin for ESP8266
-
-const int moveSteps = 200;    // test steps
-char cmd;
-int data;
-int motorSpeed = 1000;
-
 #include <SoftwareSerial.h>
 
 // For ESP8266 update pin definitions (using NodeMCU pin labels)
@@ -18,20 +9,13 @@ const int txPin = 13;    // TX pin for software serial
 const int moveSteps = 200;    // test steps
 char cmd;
 int data;
-// For ESP8266 update pin definitions (using NodeMCU pin labels)
-const int dirPin = 5;    // direction control pin for ESP8266
-const int stepPin = 4;   // step control pin for ESP8266
-const int rxPin = 12;    // RX pin for software serial
-const int txPin = 13;    // TX pin for software serial
-
-const int moveSteps = 200;    // test steps
-char cmd;
-int data;
 int motorSpeed = 1000;
 
-#include <SoftwareSerial.h>
+SoftwareSerial mySerial(rxPin, txPin);
 
-// ...rest of the code...
+void setup() {
+  pinMode(dirPin, OUTPUT);
+  pinMode(stepPin, OUTPUT);
   pinMode(rxPin, INPUT);
   pinMode(txPin, OUTPUT);
 
@@ -53,8 +37,7 @@ void loop() {
   }
 }
 
-// Updated runUsrCmd to use mySerial instead of Serial
-void runUsrCmd(){
+void runUsrCmd() {
   switch(cmd){ 
     case 'x':  
       mySerial.print("Set Rotation To ");
@@ -97,7 +80,7 @@ void runUsrCmd(){
   }
 }
 
-void runStepper (int rotationSpeed, int stepNum){
+void runStepper(int rotationSpeed, int stepNum) {
   for(int x = 0; x < stepNum; x++) {
     if (mySerial.available() && mySerial.peek() == '!') {
       mySerial.read();
@@ -110,80 +93,5 @@ void runStepper (int rotationSpeed, int stepNum){
     digitalWrite(stepPin, LOW);
     delayMicroseconds(rotationSpeed);
     yield();
-  }
-}
-
-void loop() {
-  if (Serial.available()) {
-    cmd = Serial.read();
-    Serial.print("cmd = ");
-    Serial.print(cmd);
-    Serial.print(" , ");
-    data = Serial.parseInt();
-    Serial.print("data = ");
-    Serial.println(data);
-    runUsrCmd();
-  }
-}
-
-// Updated: runUsrCmd without CNC hat commands
-void runUsrCmd(){
-  switch(cmd){ 
-    case 'x':  // Set motor rotation direction
-      Serial.print("Set Rotation To ");
-      if (data == 0){
-        digitalWrite(dirPin, HIGH);  // swapped: now HIGH means Counter Clockwise
-        Serial.println("Counter Clockwise.");
-      } else {
-        digitalWrite(dirPin, LOW);   // LOW means Clockwise
-        Serial.println("Clockwise.");
-      }
-      break;
-      
-    // New case for upward movement
-    case 'U': 
-      Serial.print("Float Up with steps = ");
-      Serial.println(data);
-      digitalWrite(dirPin, HIGH);
-      runStepper(motorSpeed, data);
-      break;
-      
-    // New case for downward movement
-    case 'D': 
-      Serial.print("Float Down with steps = ");
-      Serial.println(data);
-      digitalWrite(dirPin, LOW);
-      runStepper(motorSpeed, data);
-      break;
-      
-    case 'z': // Run stepper for a number of steps using set motor speed
-      runStepper(motorSpeed, data);
-      break;
-      
-    case 'd': // Set motor step delay speed
-      motorSpeed = data;
-      Serial.print("Set Motor Speed To ");
-      Serial.println(data);
-      break;
-      
-    case '!': // Emergency Stop command
-      Serial.println("Emergency Stop!");
-      break;
-  }
-}
-
-void runStepper (int rotationSpeed, int stepNum){
-  for(int x = 0; x < stepNum; x++) {
-    if (Serial.available() && Serial.peek() == '!') {
-      Serial.read();  // consume emergency stop command
-      Serial.println("Emergency Stop!");
-      break;
-    }
-    digitalWrite(stepPin, HIGH);
-    delayMicroseconds(rotationSpeed);
-    yield(); // allow watchdog feeding
-    digitalWrite(stepPin, LOW);
-    delayMicroseconds(rotationSpeed);
-    yield(); // allow watchdog feeding
   }
 }
